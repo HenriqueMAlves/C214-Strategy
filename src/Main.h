9f10fa0c2a5984edd82cedce13a2e35d81aea0ad
HenriqueMAlves <m.henriquealves@gmail.com>
2022-05-28 21:22:39 -0300

    feat: :sparkles: add default defines

--- src/Main.h
+++ src/Main.h
@@ -1,2 +1,10 @@
 
-void fuck(void);
+//Define para controlar o tipo de entrada: letras ("%c") ou números ("%d") 
+#define DATATYPE "%d"
+
+enum ENUM_SORT_ALGORITHM{
+	BUBBLE_SORT,
+	INSERTION_SORT,
+	QUICK_SORT,
+	NUM_MAX_ALGORITHM
+};
